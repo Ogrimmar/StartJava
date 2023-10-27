c6bb5e23fb2fae2b0022beed057c7b68eac2e269
Alexander Skvortsov <alexander.skvortsoff@gmail.com>
2023-10-27 18:35:51 +0300

    Все задания курса StartJava

--- src/com/startjava/lesson_4/guess/asterisk/GuessNumber.java
+++ src/com/startjava/lesson_4/guess/asterisk/GuessNumber.java
@@ -127,9 +127,9 @@ class GuessNumber {
     }
 
     private void swap(Player[] players, int num1, int num2) {
-        int length = players.length;
         int firstIndex = 0;
         int secondIndex = 0;
+        int length = players.length;
         for (int i = 0; i < length; i++) {
             if (i == num1) {
                 firstIndex = i;
