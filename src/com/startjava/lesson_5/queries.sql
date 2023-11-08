3e43ff4661df33a84cd0839004aa036a0191d765
Alexander Skvortsov <alexander.skvortsoff@gmail.com>
2023-11-08 13:01:59 +0300

    Все задания курса StartJava

--- src/com/startjava/lesson_5/queries.sql
+++ src/com/startjava/lesson_5/queries.sql
@@ -2,70 +2,70 @@
 
 \echo вывести всю таблицу jaegers
 SELECT *
-    FROM jaegers
+    FROM     jaegers
     ORDER BY model_name ASC;
 
 \echo вывести те строки, которые соответствуют не уничтоженным jaegers
 SELECT *
-    FROM jaegers
-    WHERE status = 'Active'
+    FROM     jaegers
+    WHERE    status = 'Active'
     ORDER BY model_name ASC;
 
 \echo вывести роботов нескольких серий: Mark-1 и Mark-4
 SELECT *
-    FROM jaegers
-    WHERE mark IN ('Mark-1', 'Mark-4')
+    FROM     jaegers
+    WHERE    mark IN ('Mark-1', 'Mark-4')
     ORDER BY model_name ASC;
 
 \echo вывести всех роботов, кроме Mark-1 и Mark-4
 SELECT *
-    FROM jaegers
-    WHERE mark NOT IN ('Mark-1', 'Mark-4')
+    FROM     jaegers
+    WHERE    mark NOT IN ('Mark-1', 'Mark-4')
     ORDER BY model_name ASC;
 
 \echo отсортируйте таблицу по убыванию по столбцу mark
 SELECT *
-    FROM jaegers
+    FROM     jaegers
     ORDER BY mark DESC;
 
 \echo вывести информацию о самом старом роботе
 SELECT *
-    FROM jaegers
-    WHERE launch = (SELECT MIN(launch)
-                        FROM jaegers)
+    FROM     jaegers
+    WHERE    launch = (SELECT MIN(launch)
+                           FROM jaegers)
     ORDER BY model_name ASC;
 
 \echo вывести информацию о роботах, которые уничтожили больше всех kaiju
 SELECT *
-    FROM jaegers
-    WHERE kaiju_kill = (SELECT MAX(kaiju_kill)
-                            FROM jaegers)
+    FROM     jaegers
+    WHERE    kaiju_kill = (SELECT MAX(kaiju_kill)
+                               FROM jaegers)
     ORDER BY model_name ASC;
 
 \echo отобразите средний вес роботов, округлив его до трёх знаков после запятой		
 SELECT ROUND(AVG(weight), 3) AS avg_weight
-    FROM jaegers
+    FROM     jaegers
 	GROUP BY model_name
     ORDER BY model_name ASC;
 
 \echo увеличить на единицу количество уничтоженных kaiju у роботов, которые до сих пор не разрушены, а затем отобразить таблицу
 SELECT *
-    FROM jaegers
+    FROM     jaegers
     ORDER BY model_name ASC;
 
-UPDATE jaegers
-    SET kaiju_kill = kaiju_kill + 1
+UPDATE    jaegers
+    SET   kaiju_kill = kaiju_kill + 1
     WHERE status <> 'Destroyed';
 
 SELECT *
-    FROM jaegers
+    FROM     jaegers
     ORDER BY model_name ASC;
 
 \echo удалить уничтоженных роботов, а затем отобразите оставшихся
 DELETE
-    FROM jaegers
+    FROM  jaegers
     WHERE status = 'Destroyed';
 
 SELECT *
-    FROM jaegers
+    FROM     jaegers
     ORDER BY model_name ASC;
